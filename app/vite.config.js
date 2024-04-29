f09d8c9c67380b4a6bd4cc4fe25a3f08d6b43441
CrisM05 <Max42113@gmail.com>
2024-04-28 23:58:00 -0400

    Should be the final one

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // GitHub Pages expects an index.html in the root directory
-  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+  plugins: [react()],
   build: { outDir: ".." },
-  // needed for github pages just put the repo name here
   base: "/futurama-theorum/",
 });
